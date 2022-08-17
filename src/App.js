289ddd2277dcded3ec3406c8ba7a6bdd2a41ae7f
Michael Jones <iammichaeljon27@gmail.com>
2022-08-17 21:59:30 +0400

    Create dropdown popup onclick for image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import img from './images/9ufayzzd8ih91.jpg'
 
 function App() {
+
+  const [popup, setPopup] = useState(false)
+  const [coord, setCoord] = useState([0, 0])
+  function loginfo(e) {   // e = Mouse click event.
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left; //x position within the element.
+    const y = e.clientY - rect.top;  //y position within the element.
+    // console.log("Left? : " + x + " ; Top? : " + y + ".");
+
+    const bool = !popup
+    setPopup(bool)
+    setCoord([e.clientX + 15, e.clientY])
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='img-wrapper'>
+        <img src={img} onClick={loginfo}>
+        </img>
+      </div>
+      {popup ? <div className="pop" style={{ top: coord[1], left: coord[0] }}>TEST</div> : null}
+      {/* TODO: DROPDOWN DISPLAY FOR POPUP */}
+
     </div>
+
   );
 }
 
